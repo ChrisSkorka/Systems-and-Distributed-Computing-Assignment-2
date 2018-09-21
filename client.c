423688298e8498d8afc92c23f97ee02c59a0a12b
TechSupport420 <30539703+TechSupport420@users.noreply.github.com>
2018-09-21 14:30:37 +1000

    Begun progress display

--- client.c
+++ client.c
@@ -11,12 +11,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "sharedmemory.h"
+#include <pthread.h>
 
 // TYPEDEFS ////////////////////////////////////////////////////////////////////
 typedef enum { false, true } bool;
 
 // GLOBALS /////////////////////////////////////////////////////////////////////
 // PROTOTYPES //////////////////////////////////////////////////////////////////
+void main(int argc, char** argv);
+void updateProgress(void *vargp);
+
 // FUNCTIONS ///////////////////////////////////////////////////////////////////
 
 // -----------------------------------------------------------------------------
@@ -27,10 +31,10 @@ typedef enum { false, true } bool;
 // -----------------------------------------------------------------------------
 
 // -----------------------------------------------------------------------------
+// main client, manages all client related tasks
 // 
-// 
-// Parameters:	
-// Returns:		
+// Parameters:	cmd arguments
+// Returns:		void
 // -----------------------------------------------------------------------------
 void main(int argc, char** argv){
 	printf("Client\n");
@@ -38,8 +42,16 @@ void main(int argc, char** argv){
 	// struct shared with server for communication
 	Memory* server = getSharedMemory();
 
+	// start progress thread
+	// setup arguments and start
+    pthread_t* thread = (pthread_t*) calloc(1, sizeof(pthread_t));
+    pthread_attr_t* attr = (pthread_attr_t*) calloc(1, sizeof(pthread_attr_t));
+	pthread_create(thread, attr, &updateProgress, (void *) server);
+
+	// buffer for std input
 	char cmd_buffer[256] = "";
 
+	// querry for input
 	while(strcmp(cmd_buffer, "q") == 0){
 		// get next command
 		printf("> ");
@@ -50,5 +62,35 @@ void main(int argc, char** argv){
 	server->request_status = 'X';
 
 	sleep(10);
+
+	// wait for thread to finish
+	pthread_join(*thread, NULL);
+
+}
+
+// -----------------------------------------------------------------------------
+// updates the progress of the server tasks, it updates every 500ms if the user
+// is not entering a new command
+// Parameters:	void
+// Returns:		void
+// -----------------------------------------------------------------------------
+void updateProgress(void *vargp){
+
+	while(1){
+
+		// get shared memory
+		Memory* server = (Memory*) vargp;
+
+		// compute progress string
+		char* progressStr[256];
+		server->request++;
+		sprintf(progressStr, "%i", server->request);
+
+		// insert progress above user input
+		printf(progressStr);
+
+		sleep(1);
+
+	}
 
 }
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 project: client server
 
-client: client.c sharedmemory.c
+client: client.c sharedmemory.c sharedmemory.h
 	gcc -o client client.c sharedmemory.c
 
-server: server.c sharedmemory.c
+server: server.c sharedmemory.c sharedmemory.h
 	gcc -o server server.c sharedmemory.c
 	
